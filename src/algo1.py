3bd2c0cfc0bd564b970d69d52073e28393fd6c72
unknown <jan.jezersek@gmail.com>
2019-02-28 22:31:54 +0100

    shit

--- src/algo1.py
+++ src/algo1.py
@@ -7,9 +7,10 @@ Created on Thu Feb 28 19:53:21 2019
 
 from filereader import filereader
 import numpy as np
+from scipy.optimize import linear_sum_assignment
 
 if __name__ == "__main__":
-    hashes,imgs = filereader("../testcases/a_example.txt")
+    hashes,imgs = filereader("../testcases/c_memorable_moments.txt")
     Vs = imgs['V']
     Hs = imgs['H']
 
@@ -23,6 +24,9 @@ def toNs(A):
 def combine_Vs(v1,v2):
     w = list(set(v1[:-1] + v2[:-1]))
     return w
+
+def combine_Ws_with_indices():
+    pass
     
 def generate_Ws(Vs):    
     if len(Vs) % 2 == 1:
@@ -36,3 +40,69 @@ def generate_Ws(Vs):
         Ws[i] = combine_Vs(Vs[i],Vs[-(i+1)]) + [str(Vs[i][-1]) + " " + str(Vs[-(i+1)][-1])]
     
     return sorted(Ws,key=len)
+
+if 0:
+    Ws = generate_Ws(Vs)
+    
+    while True:
+        Ws = generate_Ws(Ws)
+        if len(Ws) ==  1:
+            break
+        
+    res = Ws[0][-1].split(" ")
+        
+    with open("../out/e_testJJ.txt", 'w') as f:
+        f.write("{}\n".format(len(res)//2))
+        for i in range(len(res)//2):
+            f.write("{} {}\n".format(res[2*i],res[2*i + 1]))
+            
+def dist(a,b):
+    v1 = set(a[:-1]).intersection(b[:-1])
+    v2 = len(set(a[:-1]) - v1)
+    v3 = len(set(b[:-1]) - v1)
+    return min([len(v1),v2,v3])
+
+def dist2(a, b):
+    inter = len(set(a).intersection(set(b)))
+    return min([inter, len(a)-inter, len(b)-inter])
+
+def correct_hs(Hs):
+    for i in range(len(Hs)):
+        Hs[i][-1] = str(Hs[i][-1])
+    
+if 0:
+    N_opt = 10
+    
+    Ws = generate_Ws(Vs)
+    imgs = Ws + Hs
+    
+        
+    for ind in range(0,len(imgs),N_opt):
+        vals = [[0 for i in range(N_opt)] for j in range(N_opt)]
+        for i in range(N_opt):
+            for j in range(N_opt):
+                if i == j:
+                    vals[i][j] = 0
+                d = dist(imgs[ind+i],imgs[ind+j])
+                vals[i][j] = d
+                
+        row_ind, col_ind = linear_sum_assignment(np.array(vals))
+        
+        
+                
+    
+    
+    
+    
+    
+        
+    
+        
+    
+    
+        
+        
+    
+    
+    
+    
