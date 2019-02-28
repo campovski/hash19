0f3d5bedc5d1b81db65f8801426b34f332f96a18
campovski <campovski@gmail.com>
2019-02-28 19:59:20 +0100

    Change filereader.py

--- src/filereader.py
+++ src/filereader.py
@@ -1,25 +1,31 @@
 def filereader(filename):
     hashes = {}
-    imgs = None
     M = 0
 
     f = open(filename)
     N = int(f.readline())
-    imgs = [None for _ in range(N)]
+    imgs = {
+        'H': [],
+        'V': []
+    }
 
     for i in range(N):
         line = f.readline().split(" ")
         n = int(line[1])
-        imgs[i] = [None for _ in range(n+1)]
-        for j in range(int(n)):
+        imgs[line[0]].append([None for _ in range(n+1)])
+        print(imgs)
+        for j in range(n):
             try:
-                imgs[i][j] = hashes[line[j+2].strip()]
+                imgs[line[0]][-1][j] = hashes[line[j+2].strip()]
             except:
                 hashes[line[j+2].strip()] = M
-                imgs[i][j] = M
+                imgs[line[0]][-1][j] = M
                 M += 1
-        imgs[i][-1] = line[0]
-        imgs[i][:-1] = sorted(imgs[i][:-1])
+        imgs[line[0]][-1][:-2] = sorted(imgs[line[0]][-1][:-2])
+        imgs[line[0]][-1][-1] = i
+
+    imgs['H'].sort(key=len)
+    imgs['V'].sort(key=len)
 
     return hashes, imgs
 
